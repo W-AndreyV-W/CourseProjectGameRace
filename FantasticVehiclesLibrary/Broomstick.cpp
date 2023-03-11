317f900a045780573375f614c8826a468371e4f6
Andrey V <Andreyxvv@hotmail.com>
2023-03-11 13:58:26 +0500

    corrected 2

--- FantasticVehiclesLibrary/Broomstick.cpp
+++ FantasticVehiclesLibrary/Broomstick.cpp
@@ -5,7 +5,10 @@ Broomstick::Broomstick(float distance_race, std::string name_vehicles, float spe
 
 float Broomstick::time() {
 	if (VehiclesInitial::distance() / 1000 == 0) {
-		return trunc((VehiclesInitial::time() * (100 - (trunc(VehiclesInitial::distance() / 1000) - 1) * Air::coefficient()) / 100) * 100) / 100;
+		return trunc(VehiclesInitial::time() * (100 - (trunc(VehiclesInitial::distance() / 1000 - 1)) * Air::coefficient()) / 100 * 100) / 100;
+	}
+	else {
+		return trunc(VehiclesInitial::time() * (100 - (trunc(VehiclesInitial::distance() / 1000)) * Air::coefficient()) / 100 * 100) / 100;
 	}
 }
 
